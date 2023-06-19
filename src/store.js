aa9c64faf60e5e9d05c681aad08fb136be90a979
AlexMagar <lx.mgr07@gmail.com>
2023-06-19 23:52:11 +1000

    added formReducer from the formslice

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from './counterSlice'
+import formReducer from './formSlice'
 
 // configureStore() is a function, it receive an object and object has poperty called reducer
 const store = configureStore({
     reducer:{
         counterData: counterReducer,
+        formData: formReducer,
     },
 });
 
