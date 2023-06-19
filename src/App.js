b007fe8adb0eefa134aa660e6d73c33f5aef78a7
AlexMagar <lx.mgr07@gmail.com>
2023-06-19 23:45:46 +1000

    remove the DisplayFrom component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Display } from './Components/Display';
 import { Buttons } from './Components/Buttons';
-import { useState } from 'react';
+import { FormData } from './Components/FormData';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increase = () =>{
-    setCount( count + 1)
-  }
-
-  const decrease = () => {
-    setCount( count - 1 )
-  }
   return (
     <div className="App">
-      <Display count={count}/>
-      <Buttons increase={increase} decrease={decrease}/>
+      <Display />
+      <Buttons />
+      <hr/>
+      <FormData/>
     </div>
   );
 }
