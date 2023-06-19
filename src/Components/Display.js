e6acdecd445ed148da59a65271d76d1f7e14967a
AlexMagar <lx.mgr07@gmail.com>
2023-06-19 23:47:57 +1000

    added ul for form data

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-export const Display = ({count}) => {
+export const Display = () => {
+  const {counter} = useSelector((state) => state.counterData );
+  const {userList} = useSelector((state) => state.formData);
   return (
     <div>
-        <p>{count}</p>
+        <p>{counter}</p>
+        <ul>
+          {
+            userList.map( (item) => (
+              <li>{item}</li>
+            ))
+          }
+        </ul>
     </div>
   )
 }
